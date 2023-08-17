cff90aa4aeabe2b44642182abd2f7adb2b26e043
Raja Kumar <rajakumarmahto952@gmail.com>
2023-08-17 19:06:18 +0530

    updating functionalities of find property

--- src/pages/FindProperty.js
+++ src/pages/FindProperty.js
@@ -1,18 +1,8 @@
 import React, { useState, useEffect } from "react";
+import { getDatabase, ref, get, push, set } from "firebase/database";
 import Navbar from "../Components/Navbar/Navbar";
 import Footer from "../Components/Footer/Footer";
-import {
-  getDatabase,
-  ref,
-  query,
-  orderByChild,
-  equalTo,
-  get,
-  push,
-  set,
-  remove,
-} from "firebase/database";
-import PropertyCard from "../Components/PropertyCard/PropertyCard";
+import { NavLink } from "react-router-dom";
 
 const FindProperty = () => {
   const [location, setLocation] = useState("");
@@ -20,6 +10,9 @@ const FindProperty = () => {
   const [propertyType, setPropertyType] = useState("");
   const [bedrooms, setBedrooms] = useState("");
   const [properties, setProperties] = useState([]);
+  const [matchingProperties, setMatchingProperties] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleSearch = async (e) => {
     e.preventDefault();
@@ -32,10 +25,14 @@ const FindProperty = () => {
     };
 
     try {
+      setLoading(true); // Set loading to true while fetching data
+      setError(null); // Clear any previous error
       const matchingProperties = await performPropertySearch(searchCriteria);
-      setProperties(matchingProperties);
+      setMatchingProperties(matchingProperties);
     } catch (error) {
-      console.log("Error performing search:", error);
+      setError("Error performing search");
+    } finally {
+      setLoading(false); // Set loading back to false after fetching data
     }
   };
 
@@ -43,19 +40,53 @@ const FindProperty = () => {
     try {
       const db = getDatabase();
       const propertiesRef = ref(db, "properties");
-      const queryRef = query(
-        propertiesRef,
-        orderByChild("location"),
-        equalTo(searchCriteria.location)
-      );
-      const snapshot = await get(queryRef);
-      const matchingProperties = [];
+      const snapshot = await get(propertiesRef);
+
+      const allProperties = [];
       snapshot.forEach((childSnapshot) => {
-        matchingProperties.push({
+        allProperties.push({
           id: childSnapshot.key,
           ...childSnapshot.val(),
         });
       });
+
+      // Apply client-side filtering based on the search criteria
+      const matchingProperties = allProperties.filter((property) => {
+        if (
+          searchCriteria.location &&
+          property.location !== searchCriteria.location
+        ) {
+          return false;
+        }
+
+        if (
+          searchCriteria.priceRange &&
+          property.priceRange !== searchCriteria.priceRange
+        ) {
+          return false;
+        }
+
+        if (
+          searchCriteria.propertyType &&
+          property.propertyType !== searchCriteria.propertyType
+        ) {
+          return false;
+        }
+
+        if (
+          searchCriteria.bedrooms &&
+          property.bedrooms !== searchCriteria.bedrooms
+        ) {
+          return false;
+        }
+
+        // Add other filtering conditions as needed
+
+        return true;
+      });
+
+      console.log("Matching Properties:", matchingProperties);
+
       return matchingProperties;
     } catch (error) {
       console.log("Error performing search:", error);
@@ -87,104 +118,195 @@ const FindProperty = () => {
   };
 
   useEffect(() => {
-    setProperties([]);
+    const fetchProperties = async () => {
+      try {
+        setLoading(true); // Set loading to true while fetching data
+        setError(null); // Clear any previous error
+        const db = getDatabase();
+        const propertiesRef = ref(db, "properties");
+        const snapshot = await get(propertiesRef);
+        const allProperties = [];
+        snapshot.forEach((childSnapshot) => {
+          allProperties.push({
+            id: childSnapshot.key,
+            ...childSnapshot.val(),
+          });
+        });
+        setProperties(allProperties);
+      } catch (error) {
+        setError("Error fetching properties");
+      } finally {
+        setLoading(false); // Set loading back to false after fetching data
+      }
+    };
+    fetchProperties();
   }, []);
-
-  const handleDelete = (propertiesIdToDelete) => {
-    const db = getDatabase();
-    const agentRef = ref(db, `properties/${propertiesIdToDelete}`);
-    remove(agentRef);
-  };
 
   return (
     <>
       <Navbar />
-      <div className="container mx-auto px-4 py-8">
-        <h2 className="text-2xl font-semibold mb-4">Property Search</h2>
-        <form onSubmit={handleSearch}>
-          <div className="mb-4">
-            <label htmlFor="location" className="block mb-1">
-              Location:
-            </label>
-            <input
-              type="text"
-              id="location"
-              value={location}
-              onChange={(e) => setLocation(e.target.value)}
-              className="border border-gray-300 px-2 py-1 rounded"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="priceRange" className="block mb-1">
-              Price Range:
-            </label>
-            <select
-              id="priceRange"
-              value={priceRange}
-              onChange={(e) => setPriceRange(e.target.value)}
-              className="border border-gray-300 px-2 py-1 rounded"
-            >
-              <option value="">Any</option>
-              <option value="0-100000">$0 - $100,000</option>
-              <option value="100000-200000">$100,000 - $200,000</option>
-              <option value="200000-300000">$200,000 - $300,000</option>
-            </select>
-          </div>
-          <div className="mb-4">
-            <label htmlFor="propertyType" className="block mb-1">
-              Property Type:
-            </label>
-            <select
-              id="propertyType"
-              value={propertyType}
-              onChange={(e) => setPropertyType(e.target.value)}
-              className="border border-gray-300 px-2 py-1 rounded"
-            >
-              <option value="">Any</option>
-              <option value="house">House</option>
-              <option value="apartment">Apartment</option>
-              <option value="condo">Condo</option>
-            </select>
-          </div>
-          <div className="mb-4">
-            <label htmlFor="bedrooms" className="block mb-1">
-              Bedrooms:
-            </label>
-            <select
-              id="bedrooms"
-              value={bedrooms}
-              onChange={(e) => setBedrooms(e.target.value)}
-              className="border border-gray-300 px-2 py-1 rounded"
-            >
-              <option value="">Any</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-            </select>
-          </div>
-          <button
-            type="submit"
-            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+      <div className="container flex-col mx-auto px-4 py-8">
+        <div className="flex-row">
+          <ul
+            className={`md:flex md:items-center hidden space-x-3  "block" : "hidden"
+            }`}
           >
+            <NavLink to="/">
+              <li className="text-gray-800 text-xl hover:text-gray-500 pr-4">
+                Home
+              </li>
+            </NavLink>
+            <NavLink to="/findproperty">
+              <li className="text-gray-800 text-xl hover:text-gray-500 pr-4">
+                Find Property
+              </li>
+            </NavLink>
+            <NavLink to="/listproperty">
+              <li className="text-gray-800 text-xl hover:text-gray-500 pr-2">
+                List Property
+              </li>
+            </NavLink>
+            <NavLink to="/agents">
+              <li className="text-gray-800 text-xl hover:text-gray-500 pr-2">
+                Agents
+              </li>
+            </NavLink>
+            <NavLink to="/contacts">
+              <li className="text-gray-800 text-xl hover:text-gray-500 pr-2">
+                Contact Us
+              </li>
+            </NavLink>
+          </ul>
+        </div>
+        <div className="flex justify-between items-center">
+          <input
+            type="search"
+            placeholder="Search"
+            className="border border-gray-300 px-2 py-1 rounded"
+          />
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             Search
           </button>
-          <button
-            type="button"
-            onClick={storePropertyData}
-            className="bg-green-500 text-white px-4 py-2 rounded ml-4 hover:bg-green-600"
-          >
-            Save Property
-          </button>
-        </form>
+        </div>
+      </div>
+
+      <div className="container mx-auto px-4 py-8">
+        <h2 className="text-2xl font-semibold mb-4">Property Search</h2>
+        <div className="mb-4">
+          <form onSubmit={handleSearch}>
+            <div className="mb-4">
+              <label htmlFor="location" className="block mb-1">
+                Location:
+              </label>
+              <input
+                type="text"
+                id="location"
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+                className="border border-gray-300 px-2 py-1 rounded"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="priceRange" className="block mb-1">
+                Price Range:
+              </label>
+              <select
+                id="priceRange"
+                value={priceRange}
+                onChange={(e) => setPriceRange(e.target.value)}
+                className="border border-gray-300 px-2 py-1 rounded"
+              >
+                <option value="">Any</option>
+                <option value="0-100000">$0 - $100,000</option>
+                <option value="100000-200000">$100,000 - $200,000</option>
+                <option value="200000-300000">$200,000 - $300,000</option>
+              </select>
+            </div>
+            <div className="mb-4">
+              <label htmlFor="propertyType" className="block mb-1">
+                Property Type:
+              </label>
+              <select
+                id="propertyType"
+                value={propertyType}
+                onChange={(e) => setPropertyType(e.target.value)}
+                className="border border-gray-300 px-2 py-1 rounded"
+              >
+                <option value="">Any</option>
+                <option value="house">House</option>
+                <option value="apartment">Apartment</option>
+                <option value="condo">Condo</option>
+              </select>
+            </div>
+            <div className="mb-4">
+              <label htmlFor="bedrooms" className="block mb-1">
+                Bedrooms:
+              </label>
+              <select
+                id="bedrooms"
+                value={bedrooms}
+                onChange={(e) => setBedrooms(e.target.value)}
+                className="border border-gray-300 px-2 py-1 rounded"
+              >
+                <option value="">Any</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+              </select>
+            </div>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+            >
+              Search
+            </button>
+            <button
+              type="button"
+              onClick={storePropertyData}
+              className="bg-green-500 text-white px-4 py-2 rounded ml-4 hover:bg-green-600"
+            >
+              Save Property
+            </button>
+          </form>
+        </div>
 
         <div className="mt-8">
-          {properties.length === 0 ? (
-            <p>No properties found.</p>
-          ) : (
-            properties.map((property) => (
-            <PropertyCard key={property.id} property={property} onDelete={handleDelete} />
-            ))
-          )}
+          {/* Render loading message while fetching data */}
+          {loading && <p>Loading...</p>}
+
+          {/* Render error message if there was an error */}
+          {error && <p>{error}</p>}
+
+          {/* Conditionally render properties based on whether a search is performed */}
+          {matchingProperties.length === 0
+            ? properties.map((property) => (
+                <div
+                  key={property.id}
+                  className="border border-gray-200 rounded p-4 mb-4"
+                >
+                  <h2 className="text-xl font-bold mb-2">
+                    {property.location}
+                  </h2>
+                  <p className="text-gray-600 mb-2">
+                    Price: {property.priceRange}
+                  </p>
+                  <p className="text-gray-600 mb-2">
+                    Type: {property.propertyType}
+                  </p>
+                  <p className="text-gray-600 mb-2">
+                    Bedrooms: {property.bedrooms}
+                  </p>
+                </div>
+              ))
+            : matchingProperties.map((property) => (
+                <div key={property.id} className="bg-gray-100 p-4 mb-4">
+                  <p className="mb-2">Price: {property.priceRange}</p>
+                  <p className="mb-2">Location: {property.location}</p>
+                  <p className="mb-2">Type: {property.propertyType}</p>
+                  <p className="mb-2">Bedrooms: {property.bedrooms}</p>
+                  {/* ... Render other property details ... */}
+                </div>
+              ))}
         </div>
       </div>
       <Footer />
